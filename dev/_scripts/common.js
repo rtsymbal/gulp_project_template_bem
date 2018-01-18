097705f948a1c072d1de7c1692791bfe0b750783
Roman Tsymbal <roman.tsymbal@gmail.com>
2018-01-18 23:57:34 +0200

    update

---
+++ dev/_scripts/common.js
@@ -0,0 +1,13 @@
+// ---------Put first word in span
+
+// $('.someclass').each(function() {
+// 	var ths = $(this);
+// 	ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+// });
+
+// ---------Put last word in span
+
+// $('.someclass').each(function() {
+// 	var ths = $(this);
+// 	ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+// });
